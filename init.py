81de91c414dc971f790dbe8072c84f2e80f07582
isman7 <isman7@gmail.com>
2016-03-05 17:20:23 +0100

    Button added

--- init.py
+++ init.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt4 import QtGui
-
-app = QtGui.QApplication(sys.argv)
-
-window = QtGui.QWidget()
-window.setGeometry(250, 250, 500, 300)
-window.setWindowTitle('Tutorial PyQt4')
+from PyQt4 import QtGui, QtCore
 
 
-window.show()
+class Window(QtGui.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(250, 250, 500, 300)
+        self.setWindowTitle('Tutorial PyQt4')
+        self.home()
+    
+    def home(self):
+        btn = QtGui.QPushButton("Quit", self)
+        btn.clicked.connect(self.close_app)
+        self.showFullScreen()
+        
+    def close_app(self):
+        print("Adeu")
+        sys.exit()
+
+
+
+def run():
+    
+    app = QtGui.QApplication(sys.argv)
+    GUI = Window()
+    sys.exit()
+
+run()
