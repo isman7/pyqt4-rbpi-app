205b24a747c6dd7115fa114976439a628182c833
isman7 <isman7@gmail.com>
2016-03-05 17:42:39 +0100

    Added color picker

--- init.py
+++ init.py
@@ -13,7 +13,39 @@ class Window(QtGui.QMainWindow):
     def home(self):
         btn = QtGui.QPushButton("Quit", self)
         btn.clicked.connect(self.close_app)
+        btn.move(300, 300)
+        
+        color = QtGui.QColor(0,0,0)
+        font_color = QtGui.QAction('Font bg color', self)
+        font_color.triggered.connect(self.color_picker)
+        self.toolBar = self.addToolBar("Font")
+        self.toolBar.addAction(font_color)
+
+        self.styleChoice = QtGui.QLabel("Windows Vista", self)
+
+        comboBox = QtGui.QComboBox(self)
+        comboBox.addItem("motif")
+        comboBox.addItem("Windows")
+        comboBox.addItem("cde")
+        comboBox.addItem("Plastique")
+        comboBox.addItem("Cleanlooks")
+        comboBox.addItem("windowsvista")
+
+        comboBox.move(50, 250)
+        self.styleChoice.move(50,150)
+        comboBox.activated[str].connect(self.style_choice)
+
+        
+        
         self.showFullScreen()
+        
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
+        
+    def color_picker(self):
+        color = QtGui.QColorDialog.getColor()
+        self.styleChoice.setStyleSheet("QWidget { background-color: %s}" % color.name())
         
     def close_app(self):
         print("Adeu")
